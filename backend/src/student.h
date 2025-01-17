d8f499e0ffbde549a905b3fa0eddf8e8e853fa9b
Valentin <43043885+itsmrval@users.noreply.github.com>
2025-01-17 09:45:53 +0100

    fix(backend) merge conflicts

--- backend/src/student.h
+++ backend/src/student.h
@@ -13,13 +13,4 @@ bool add_student(Student* student);
 bool get_student(int id, char* name);
 bool get_student_list(char* result);
 bool delete_student(int id);
-=======
-// Function to add a student
-bool add_student(int id, const char* name);
-
-// Function to retrieve a student by ID
-bool get_student(int id, char* name);
-
-bool get_student_list(char* result);
-
 #endif // STUDENT_H
